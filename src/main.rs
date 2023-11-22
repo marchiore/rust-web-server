db6f56c64b4fa17f7f71dc15d7832fd47af4a5a9
Matheus Z Marchiore <marchiore.matheus@gmail.com>
2023-11-21 21:43:01 -0300

    wip: adding threads to web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream}, fs,
+};
+
+use rust_web_server::ThreadPool;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        
+        pool.execute(|| {
+            handle_connection(stream);
+        });        
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            std::thread::sleep(std::time::Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{}\r\nContent-Length: {}\r\n\r\n{}", status_line, length, contents);
+        
+    stream.write(response.as_bytes()).unwrap();
 }
